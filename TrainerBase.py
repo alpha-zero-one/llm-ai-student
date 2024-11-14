8548e4afd085bf47e3dd7dc4855dce0b2ecf9865
alpha-zero-one <andreas.lizenberger.code@gmail.com>
2024-11-14 19:32:54 +0100

    Force bf16

--- TrainerBase.py
+++ TrainerBase.py
@@ -46,7 +46,7 @@ class TrainerBase:
             load_in_4bit=True,
             bnb_4bit_use_double_quant=True,
             bnb_4bit_quant_type="nf4",
-            bnb_4bit_compute_dtype=torch.float16
+            bnb_4bit_compute_dtype=torch.bfloat16
         )
 
         model = AutoModelForCausalLM.from_pretrained(
@@ -91,7 +91,7 @@ class TrainerBase:
             warmup_steps=10,
             eval_strategy="epoch",
             fp16=False,
-            bf16=False
+            bf16=True
         )
         trainer = SFTTrainer(
             model=peft_model,
